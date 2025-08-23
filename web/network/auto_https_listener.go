f12423319671781ab5f6129a0421033498541a45
xinsuiyuandong <xinsuiyuandong@gmail.com>
2025-08-24 02:04:08 +0800

    Update auto_https_listener.go

--- web/network/auto_https_listener.go
+++ web/network/auto_https_listener.go
@@ -1,38 +1,21 @@
 package network
 
-import (
-	"net"
-)
+import "net"
 
-// AutoHttpsListener 包装 net.Listener，支持每个新连接调用回调
 type AutoHttpsListener struct {
 	net.Listener
-	onAccept func(conn net.Conn) error // 新连接回调，用于设备限制检查
 }
 
-// NewAutoHttpsListener 构造函数，传入原始 listener 和回调
-func NewAutoHttpsListener(listener net.Listener, onAccept func(conn net.Conn) error) net.Listener {
+func NewAutoHttpsListener(listener net.Listener) net.Listener {
 	return &AutoHttpsListener{
 		Listener: listener,
-		onAccept: onAccept,
 	}
 }
 
-// Accept 每当有新连接进来时调用
 func (l *AutoHttpsListener) Accept() (net.Conn, error) {
 	conn, err := l.Listener.Accept()
 	if err != nil {
 		return nil, err
 	}
-
-	// 调用回调进行设备限制检查
-	if l.onAccept != nil {
-		if err := l.onAccept(conn); err != nil {
-			// 超过限制直接关闭连接
-			conn.Close()
-			return nil, err
-		}
-	}
-
-	return conn, nil
+	return NewAutoHttpsConn(conn), nil
 }
